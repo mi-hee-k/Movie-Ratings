77bd5ce6eee8e3f0ddd96ada36b786ead834c140
mi-hee-k <90510192+mi-hee-k@users.noreply.github.com>
2023-10-18 17:55:03 +0900

    데이터 가져오기, card 추가하기, api 키 숨기기

---
+++ main.js
@@ -0,0 +1,58 @@
+const form = document.querySelector('form');
+const searchInput = document.querySelector('input');
+const btn = document.querySelector('button');
+const cardSection = document.querySelector('.card-section');
+
+// const Api_Key = config.apikey;
+const ReadToken = config.readToken;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const searchValue = searchInput.value;
+  const url = `https://api.themoviedb.org/3/search/movie?query=${searchValue}&include_adult=false&language=en-US&page=1`;
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: `Bearer ${ReadToken}`,
+    },
+  };
+
+  fetch('dummy.json', options)
+    .then((res) => res.json())
+    .then((json) => {
+      let movieData = json.results;
+      makeCard(movieData);
+    })
+    .catch((err) => console.error('error:' + err));
+});
+
+const makeCard = (movieData) => {
+  cardSection.innerHTML = movieData
+    .map((item) => {
+      return `<div class="card">
+          <div class="img-box">
+            <img
+              src="https://image.tmdb.org/t/p/w500/lyQBXzOQSuE59IsHyhrp0qIiPAz.jpg"
+              alt=""
+            />
+          </div>
+          <div class="movie-card">
+            <h2 class="title">${
+              item.original_title.length > 60
+                ? item.original_title.substr(0, 60) + '...'
+                : item.original_title
+            }</h2>
+            <p class="desc">
+              ${
+                item.overview.length > 150
+                  ? item.overview.substr(0, 150) + '...'
+                  : item.overview
+              }
+            </p>
+            <span class="rating">평점 ${item.vote_average}</span>
+          </div>
+        </div>`;
+    })
+    .join('');
+};
